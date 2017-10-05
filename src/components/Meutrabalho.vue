478668ede123b3240cc1a41110cb9f87a49ed244
vainawebcpx <vainawebcpx@gmail.com>
2017-10-05 17:09:02 -0300

    pakera-att

--- src/components/Meutrabalho.vue
+++ src/components/Meutrabalho.vue
@@ -3,30 +3,89 @@
 	  <header class="Pakera">
 		 <h1>Pakera</h1>
 	   </header>
- <body>
-	 <main>
-		<button>Levar Para tomar açaí</button>
-		<button>Falar palavrão</button>  <button>Trair</button>
-		<button>Levar no Cinema</button>
-		<button> Pagar combo no Cinema</button>
-		<button>Brigar em público</button> <button>Agredir</button>
-		<button>Sua Pontuação é 29</button>
-	 </main>
- </body>
+	<main class="main">
+		<button class="intes" v-on:click="Bom">Levar Para tomar açaí</button>
+		<button class="intes" v-on:click="MuitoRuim" >Falar palavrão</button> 
+		<button class="intes" v-on:click="Ruim">Trair</button>
+		<button class="intes" v-on:click="MuitoBom">Levar no Cinema</button>
+		<button class="intes" v-on:click="Legal">Pagar combo no Cinema</button>
+		<button class="intes" v-on:click="Ridiculo">Brigar em público</button>
+	 	<button class="intes" v-on:click="Mal">Agredir</button>
+	 	<button class="intes2" v-on:click="Zerar">x</button>
+	 	<h2 class="pontos">Sua Pontuação {{ pontos }} </h2>
+	</main>
     </div>
 
 </template>
 <script>
 export default {
-  name: 'hello',
+  name: 'Meutrabalho',
   data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+    return{
+     	pontos:0
+     	}
+    },
+    methods:{
+      Bom(){
+      	this.pontos += +5
+      },
+      MuitoRuim(){
+      	this.pontos += -4
+      },
+      Ruim(){
+      	this.pontos += -3
+      },
+      MuitoBom(){
+      	this.pontos += +10
+      },
+      Legal(){
+      	this.pontos += +5
+      },
+      Ridiculo(){
+      	this.pontos += -9
+      },
+      Mal(){
+      	this.pontos += -10
+      },
+      x(){
+      	this.pontos += -0
+          }
+
+      }
+  }  
+
  
 </script>
 <style scoped>
-	 
+.Pakera{
+	color:#000;
+	font-size:20px;
+	
+
+}
+.intes{
+	color:#000;
+	width:35%; 
+	height:40px;
+	font-size:14px;
+	margin: 2%;
+	border-radius: 15px;
+
+}
+.intes2{
+	border-radius: 50%;
+}
+.pontos{
+	color:#000;
+	font-size:30px;
+
+}
+.main{
+	display:flex;
+	justify-content:space-between;
+	align-items: center;
+	flex-direction: column;
+
+}
+
 </style>
